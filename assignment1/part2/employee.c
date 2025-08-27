e936237b8e2cd6cad938a19f3407e5f92055c786
Eliseo Padilla <padillaeliseo20@gmail.com>
2025-08-27 17:58:54 -0400

    Add Assignment 1: VM setup and C programs

---
+++ assignment1/part2/employee.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char name[50];
+	int employee_id;
+	float hours_worked;
+
+	printf("OwlTech Employee Registration\n");
+	printf("=============================\n");
+
+	printf("Enter your name: ");
+	fgets(name, sizeof(name), stdin);
+	name[strcspn(name, "\n")] = '\0';
+
+	printf("Enter your employee ID: ");
+	scanf("%d", &employee_id);
+
+	//getchar();
+	
+	printf("Hours worked this week: ");
+	scanf("%f", &hours_worked);
+
+	printf("\nEmployee Summary:\n");
+	printf("Name: %s\n", name);
+	printf("ID: %d\n", employee_id);
+	printf("Hours: %.2f\n", hours_worked);
+
+	return 0;
+}
---
+++ assignment1/part2/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+	printf("Welcome to OwlTech Industries!\n");
+	printf("Systems Programming Division\n");
+
+	return 0;
+}
